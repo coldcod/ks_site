6d4eac60544d5edd2285edef7842c89a68dad270
satwinderpal <satwindersapra@gmail.com>
2017-08-19 17:02:18 +0530

    Polished Product model, possibly final

--- store/models.py
+++ store/models.py
@@ -1,34 +1,30 @@
 from django.db import models
 from django.dispatch import receiver
 from datetime import datetime
-import django
+from django.template.defaultfilters import slugify
+from django_extensions.db.fields import AutoSlugField
 
 # Create your models here.
 
 class Product(models.Model):
-
-    def gen_pid(self):
-        return self.title.replace(" ", "-") + str(self.id)
-
     description = models.CharField(max_length=600)
     title = models.CharField(max_length=600)
     price = models.IntegerField()
-    daysBeforeShipping = models.IntegerField()
+    daysBeforeShipping = models.CharField(max_length=10)
     img = models.ImageField()
-    pubdate = models.DateTimeField(default=django.utils.timezone.now)
-    stock = models.IntegerField()
+    pubdate = models.DateTimeField(default=datetime.now())
+    stock = models.IntegerField(default=2)
+    pid = models.SlugField(default=slugify(" "))
+    def save(self, *args, **kwargs):
+        def_slug = AutoSlugField(('def_slug'), max_length=10, unique=True, populate_from=('title'))
+        self.pid = slugify(def_slug)
+        super(Product, self).save(*args, **kwargs)
 
-    def __str__(self):
-        return str(self.title) + " | Rs. " + str(self.price)
-    '''def __init__(self, *args, **kwargs):
-        self.pid = self.title.replace(" ", "-") + str(self.id)
-        super(Product, self).__init__(*args, **kwargs)'''
-
-    # create staticfiles and look up db storage (check credentials, look for similar errors) figure out pid
-
-@receiver(models.signals.post_save, sender=Product, dispatch_uid="generate_pid")
-def generate_pid(sender, **kwargs):
+'''@receiver(models.signals.post_save, sender=Product, dispatch_uid="generate_pid")
+def gen_pid(sender, **kwargs):
     if kwargs.get('created', False):
         instance = kwargs.get('instance')
-        instance.pid = instance.title.replace(" ", "-") + str(instance.id)
-        instance.save()
+        instance.pid = instance.title.replace(" ", "-") + instance.id()
+        instance.save()'''
+
+    # create staticfiles and look up db storage (check credentials, look for similar errors) figure out pid
