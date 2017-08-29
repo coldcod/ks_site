cac0cd07316919ab0671093c17a2566aafcb0475
coldcod <satwindersapra@ymail.com>
2017-08-29 22:52:29 +0530

    --

--- cart/models.py
+++ cart/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 from django.utils import timezone
-
 from django.contrib.auth.models import User
 import sys
-
 sys.path.append('../')
-
 from store.models import Product
 
 # Create your models here.
