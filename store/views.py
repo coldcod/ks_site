445a1a1038b0d292aa5931f0f9e94a8033b145e3
coldcod <satwindersapra@ymail.com>
2017-09-02 19:18:43 +0530

    Cart on appropriate pages

--- store/views.py
+++ store/views.py
@@ -1,49 +1,20 @@
 from django.shortcuts import render
 from .models import Product
-
-import sys
-sys.path.append('../')
-from store.models import Product
-
-from cart.models import Cart, ProductOrder
+from get_cart_kssite import get_cart
 
 # Create your views here.
 
 def index(req):
-    boolean = req.user.is_authenticated() and str(req.user.is_anonymous) == "CallableBool(False)"
-    cart = Cart.objects.filter(user=req.user, session_id=None) if boolean else Cart.objects.filter(session_id=req.session.session_key, user=None)
-    orders = ProductOrder.objects.filter(cart=cart)
-    total = 0
-    count = 0
-    for order in orders:
-        total += order.product.price * order.quantity
-        count += order.quantity
-    orders = {
-        'orders': orders,
-        'total': total,
-        'count': count,
-    }
+    orders = get_cart.get_cart_info(req)
     latest_products = Product.objects.order_by('-pubdate')
     context = {
         'latest_products': latest_products,
-        'orders': orders,
+        'cart_info': orders,
     }
     return render(req, 'store/index.html', context)
 
 def product_page(req, pid_url):
-    boolean = req.user.is_authenticated() and str(req.user.is_anonymous) == "CallableBool(False)"
-    cart = Cart.objects.filter(user=req.user, session_id=None) if boolean else Cart.objects.filter(session_id=req.session.session_key, user=None)
-    orders = ProductOrder.objects.filter(cart=cart)
-    total = 0
-    count = 0
-    for order in orders:
-        total += order.product.price * order.quantity
-        count += order.quantity
-    cart_info = {
-        'orders': orders,
-        'total': total,
-        'count': count,
-    }
+    cart_info = get_cart.get_cart_info(req)
     product = Product.objects.get(pid=pid_url)
     top_message = req.GET.get('tm', '').replace('/', '')
     context = {
