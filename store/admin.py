d96292011d6f4625e9e62b51bb80b8d098cd75d1
coldcod <satwindersapra@ymail.com>
2017-10-20 18:04:25 +0530

    --

--- store/admin.py
+++ store/admin.py
@@ -8,10 +8,10 @@ class ProductImagesInline(admin.TabularInline):
     extra = 5
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'price', 'stock', 'pubdate', 'pid', 'category']
+    list_display = ['title', 'price', 'discount_in_percent','stock', 'pubdate', 'pid', 'category']
     list_filter = ['pubdate']
     inlines = [ProductImagesInline]
-    fields = ['description', 'title', 'category','price', 'daysBeforeShipping', 'img', 'stock']
+    fields = ['description', 'title', 'category', 'price', 'discount_in_percent','daysBeforeShipping', 'img', 'stock']
     #list_display = ['image']
 
 admin.site.register(Product, ProductAdmin)
--- store/models.py
+++ store/models.py
@@ -11,12 +11,14 @@ from .pid import pid
 class Product(models.Model):
     description = models.CharField(max_length=600)
     title = models.CharField(max_length=600)
+    og_price = models.IntegerField(default=0)
     price = models.IntegerField()
     daysBeforeShipping = models.CharField(max_length=10)
     img = models.ImageField(null=True, blank=True)
     pubdate = models.DateTimeField(default=timezone.now)
     stock = models.IntegerField(default=2)
     category = models.CharField(max_length=200)
+    discount_in_percent = models.IntegerField(default=0)
     pid = models.SlugField(default=slugify(" "), null=True, blank=True, unique=True)
     def __str__(self):
         return self.title
@@ -26,6 +28,8 @@ class Product(models.Model):
         self.pid = hash_pid
         self.title = self.title.title()
         self.category = self.category.title()
+        self.og_price = self.price
+        self.price -= (self.discount_in_percent / 100.0) * self.price
         super(Product, self).save(*args, **kwargs)
 
 class ProductImages(models.Model):
