a972746f7861a8356412db48780dd57f8442c42c
satwinderpal <satwindersapra@ymail.com>
2018-03-31 01:15:04 +0530

    --

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from django.core.urlresolvers import reverse
+#from django.core.urlresolvers import reverse
 from django.views.generic.base import RedirectView
 from . import views
 
